6b0e8272e3fcc0434410319ac47afc7767e8780a
Lixquid <lixquid@lixquid.com>
2024-03-17 15:37:53 +0000

    Fix links not working in development server

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,33 @@
-import { readdirSync } from "fs";
+import { readdirSync, stat } from "fs";
 import { join, resolve } from "path";
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
+
+/** Redirects all requests from `/foo` to `/foo/` if `out/foo` is a
+ * directory. */
+function redirectMissingTrailingSlash(): PluginOption {
+    return {
+        name: "redirect-missing-trailing-slash",
+        configureServer({ middlewares }) {
+            middlewares.use((req, res, next) => {
+                if (req.url?.endsWith("/")) {
+                    next();
+                    return;
+                }
+
+                const path = join(__dirname, "out", req.url!);
+                stat(path, (err, stats) => {
+                    if (!err && stats.isDirectory()) {
+                        res.statusCode = 302;
+                        res.setHeader("Location", req.url! + "/");
+                        res.end();
+                        return;
+                    }
+                    next();
+                });
+            });
+        },
+    };
+}
 
 // Find all *.html files in out-staging and mark them as entrypoints
 function recursiveSearch(dir: string): string[] {
@@ -19,6 +46,7 @@ const entryFiles = recursiveSearch(join(__dirname, "out"));
 
 export default defineConfig({
     root: "out",
+    plugins: [redirectMissingTrailingSlash()],
     build: {
         rollupOptions: {
             input: entryFiles.reduce((acc, path, i) => {
